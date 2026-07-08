5768aca92d9251f3daaf2efd54cbb3208b601982
Talha11998 <ktalhahussain98@gmail.com>
2026-07-08 13:00:51 +0530

    changes in db file

--- Expense_tracker-main/util/database.js
+++ Expense_tracker-main/util/database.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
   host: process.env.RDS_ENDPOINT,
+  port: process.env.DB_PORT,
   dialect:process.env.DIALECT,
 });
 
