efa6984dd034ce22493407e4c9864f7faf0ba0ac
Talha11998 <ktalhahussain98@gmail.com>
2026-07-08 12:22:08 +0530

    changes

--- Expense_tracker-main/services/cashfreeService.js
+++ Expense_tracker-main/services/cashfreeService.js
@@ -2,43 +2,8 @@ const { Cashfree, CFEnvironment } = require("cashfree-pg");
 
   const cashfree = new Cashfree(CFEnvironment.SANDBOX, "TEST430329ae80e0f32e41a393d78b923034", "TESTaf195616268bd6202eeb3bf8dc458956e7192a85");
 
-    // exports.createOrder = async (
-    //   orderId,
-    //   orderAmount,
-    //   orderCurrency="INR",
-    //   customerID,
-    //   customerPhone
-    // ) => {
-    //   try {
 
-    //     const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour from now
-    //     const formattedExpiryDate = expiryDate.toISOString();
-
-    //     const request = {
-    //       order_amount: orderAmount,
-    //       order_currency: orderCurrency,
-    //       order_id: orderId,
-          
-    //       customer_details: {
-    //         customer_id: customerID,  
-    //         customer_phone: customerPhone,
-    //       },
-
-    //       order_meta: {
-    //         // "return_url": "https://www.cashfree.com/devstudio/preview/pg/web/checkout?order_id={order_id}",
-    //         "return_url":"http://localhost:5173/payment-status/"+orderId,
-    //         payment_methods: "ccc, upi, nb"
-    //       },
-    //       order_expiry_time: formattedExpiryDate, //!? Set the valid expiry date
-    //     };
-
-    //     const response = await cashfree.PGCreateOrder(request);
-    //     return response.data.payment_session_id;
-    //   } catch (error) {
-    //     console.error("Error creating order:", error.message);
-    //   }
-    // };
-    exports.createOrder = async (
+  exports.createOrder = async (
   orderId,
   orderAmount,
   orderCurrency = "INR",
@@ -95,7 +60,7 @@ const { Cashfree, CFEnvironment } = require("cashfree-pg");
   }
 };
 
-    exports.getPaymentStatus = async (orderId) => {
+ exports.getPaymentStatus = async (orderId) => {
       try {
         const response = await cashfree.PGOrderFetchPayments(orderId);
 
